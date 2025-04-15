fc9a2ddb81817208349c395725695eb623006588
Liendson <liendsondouglas1@gmail.com>
2025-04-15 13:45:24 -0300

    feat: Adicionando TimeZone Default no init()

--- src/main/java/br/com/yugiapp/YuGiAppApplication.java
+++ src/main/java/br/com/yugiapp/YuGiAppApplication.java
@@ -1,8 +1,12 @@
 package br.com.yugiapp;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+import java.time.LocalDateTime;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableWebSocketMessageBroker
@@ -10,6 +14,12 @@ public class YuGiAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YuGiAppApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+		System.out.println(LocalDateTime.now());
 	}
 
 }
